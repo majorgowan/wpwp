d6c0a1360281509384df1c7ecd1f172d408f71d7
Mark Fruman <majorgowan@yahoo.com>
2015-11-05 18:12:14 -0500

    lots of progress on regression model

--- Scraping/wURegression.py
+++ Scraping/wURegression.py
@@ -2,9 +2,7 @@
 ################## REGRESSION MODEL ###########################
 ###############################################################
 #
-
-#
-def loadDailyVariableRange(station, startdate, enddate, \
+def loadDailyVariableRange(station, startDate, endDate, \
                            variable, castFloat=False):
      vals = []
      with open('CSV_DATA/' + station + '.csv','r') as infile:
@@ -15,72 +13,117 @@ def loadDailyVariableRange(station, startdate, enddate, \
           for nline in infile:
                nlist = nline.strip().split(', ')
                nday = nlist[datepos]
-               if nday == startdate:
+               if nday == startDate:
                     recording = True
                if recording:
                     if castFloat:
                          vals.append(float(nlist[varpos]))
                     else:
                          vals.append(nlist[varpos])
-               if nday == enddate:
+               if nday == endDate:
                     recording = False
                     break
      return vals
 
+def dateList(startDate, endDate):
+     import datetime
+     day0 = datetime.datetime.strptime(startDate,"%Y-%m-%d")
+     dayN = datetime.datetime.strptime(endDate,"%Y-%m-%d")
+     N = (dayN-day0).days
+     date_list = [day0 + datetime.timedelta(days = n) for n in range(0,N+1)]
+     return date_list
+     
 #
-def oneCityModel(station, startdate, enddate, \
+def oneCityModel(station, startDate, endDate, \
                  features, targetVar='TempMax', lag=1):
      # build regression model to predict "variable" for a single
      # station using training data from only the same station 
      # between startdate and enddate
      # features is a list of variables to use as predictors
-     import wUStats as Stats
      import numpy as np
      from sklearn import linear_model
      # load target variable data
-     target = loadDailyVariableRange(station, startdate, enddate, \
-                        targetVar, castFloat=True) 
+     target = loadDailyVariableRange(station, startDate, endDate, \
+                        targetVar, castFloat=True)
+     # shift vector by lag
+     target = target[lag:]
      # load feature data
+     featureData = []
      for feature in features:
-          print(feature)
-'''              
-regr = linear_model.LinearRegression()
-targ = Reg.loadDailyVariableRange('CYYZ', '2006-01-01', '2010-12-31', 'TempMax', castFloat=True)
-torMin = Reg.loadDailyVariableRange('CYYZ', '2006-01-01', '2010-12-31', 'TempMin', castFloat=True)
-torMax = Reg.loadDailyVariableRange('CYYZ', '2006-01-01', '2010-12-31', 'TempMax', castFloat=True)
-torHum = Reg.loadDailyVariableRange('CYYZ', '2006-01-01', '2010-12-31', 'HumidityMean', castFloat=True)
-torPressMax = Reg.loadDailyVariableRange('CYYZ', '2006-01-01', '2010-12-31', 'PressMax', castFloat=True)
-torPressMin = Reg.loadDailyVariableRange('CYYZ', '2006-01-01', '2010-12-31', 'PressMin', castFloat=True)
-targ = torMax[1:]
-targ = np.array(torMax[1:])
-v1 = np.array(torMax[:-1])
-ft_Tmax = np.array(torMax[:-1])
-ft_Tmin = np.array(torMin[:-1])
-ft_Pmin = np.array(torPressMin[:-1])
-ft_Pmax = np.array(torPressMax[:-1])
-ft_Hum = np.array(torHum[:-1])
-regr = linear_model.LinearRegression()
-help(regr.fit)
-feat = np.vstack(ft_Tmax,ft_Tmin,ft_Pmax,ft_Pmin,ft_Hum).T
-feat = np.vstack((ft_Tmax,ft_Tmin,ft_Pmax,ft_Pmin,ft_Hum)).T
-size(feat)
-np.size(feat)
-feat.size
-feat.shape
-regr.fit(feat, targ)
-print('Coefficients: \n', regr.coef_)
-import matplotlib.pyplot as plt
-pred = regr.predict(feat)
-plt.scatter(feat, targ,  color='black')
-plt.scatter(range(len(targ)), targ,  color='black')
-plt.scatter(range(len(targ)), pred,  color='red')
-plt.scatter(range(len(targ)), targ,  color='black')
-plt.plot(range(len(targ)), pred,  color='red')
-ff = array()
-ff = np.array()
-ff = np.array([])
-ff = np.vstack(ff,ft_Hum).T
-ff = np.vstack((ff,ft_Hum)).T
-ff = np.vstack((ft_Hum)).T
-ff = np.vstack((ft_Hum,ft_Hum)).T
-'''
+          # print("Adding " + feature)
+          fd = loadDailyVariableRange(station, startDate, endDate, \
+                        feature, castFloat=True)
+          # shorten vector by lag
+          fd = fd[:(-lag)]
+          featureData.append(fd)
+     # convert target and features to np arrays
+     target = np.array(target)
+     featureData = (np.array(featureData)).T
+     regr = linear_model.LinearRegression()
+     regr.fit(featureData, target)
+     model_params = {
+            'station': station, \
+            'startDate': startDate, \
+            'endDate': endDate, \
+            'targetVar': targetVar, \
+            'features': features, \
+            'lag': lag }
+     # report regression results:
+     print("R^2: " + str(regr.score(featureData,target)))
+     print("Regression coefficients:")
+     print("  intercept" + ":\t" + str(regr.intercept_))
+     for ii, feature in enumerate(features):
+          print("  " + feature + ":\t" + str(regr.coef_[ii]))         
+     return featureData, target, regr, model_params
+
+#
+def oneCityPredict(regr, model_params, startDate, endDate, actual=True):
+     # predict targetVar for a single station using 
+     # previously generated regression model
+     import numpy as np
+     # extract city and feature data
+     station = model_params['station']
+     targetVar = model_params['targetVar']
+     features = model_params['features']
+     lag = model_params['lag']
+     # build list of dates in datetime format
+     date_list = dateList(startDate, endDate)
+     date_list = date_list[lag:]
+     # if actual data available
+     if actual:
+          # load target variable data
+          target = loadDailyVariableRange(station, startDate, endDate, \
+                             targetVar, castFloat=True)
+          # shift vector by lag
+          target = target[lag:]
+          target = np.array(target)
+     else:
+          target = None
+     # load feature data
+     featureData = []
+     for feature in features:
+          # print("Adding " + feature)
+          fd = loadDailyVariableRange(station, startDate, endDate, \
+                        feature, castFloat=True)
+          # shorten vector by lag
+          fd = fd[:(-lag)]
+          featureData.append(fd)
+     # convert target and features to np arrays
+     featureData = (np.array(featureData)).T
+     pred = regr.predict(featureData)
+     return date_list, pred, target
+
+#
+def plotModelPred(date_list, pred, target):
+     import matplotlib.pyplot as plt
+     # create new figure
+     fig = plt.figure()
+     # line and scatter plot of true values
+     plt.plot(date_list, target, color='black')
+     plt.scatter(date_list, target, color='black')
+     # line and scatter plot of predictions
+     plt.plot(date_list, pred, color='red')
+     plt.scatter(date_list, pred, color='red')
+     plt.show()
+     
+     
