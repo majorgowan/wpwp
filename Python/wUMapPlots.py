b9a5367a5f3f02b44329ca8cb8990e6a7b7a9c83
Mark Fruman <majorgowan@yahoo.com>
2015-11-23 15:32:13 -0500

    kicked out of lab ... coming back later

--- Python/wUMapPlots.py
+++ Python/wUMapPlots.py
@@ -8,34 +8,44 @@
 ################# PLOT POINTS AND LABELS ON MAP ###############
 ###############################################################
 #
-def plotOnMap():
+def plotStationsOnMap(showIt=True):
      import wUUtils as Util
      from mpl_toolkits.basemap import Basemap
      import matplotlib.pyplot as plt
      # setup Lambert Conformal basemap.
      m = Basemap(width=3200000,height=2500000,projection='lcc',
-            resolution='i',lat_1=45.,lat_0=43.6,lon_0=-80.)
+            resolution='i',lat_1=45.,lat_0=43.6,lon_0=-81.)
      # draw coastlines.
-     m.drawcoastlines()
-     m.drawcountries()
-     m.drawstates()
+     m.drawcoastlines(color='gray')
+     m.drawcountries(color='gray')
+     m.drawstates(color='gray')
      # draw a boundary around the map, fill the background.
      # this background will end up being the ocean color, since
      # the continents will be drawn on top.
-     m.drawmapboundary(fill_color='aqua')
+     m.drawmapboundary(fill_color='lightblue')
      # fill continents, set lake color same as ocean color.
-     m.fillcontinents(color='wheat',lake_color='aqua')
-     # plot city locations (Toronto, Montreal, Detroit)
+     m.fillcontinents(color='navajowhite',lake_color='lightblue')
+     # plot city locations
      cityName = Util.getStationList()
      lon, lat = Util.getStationLonLat(cityName)
-     # lon, lat = -79.6306, 43.6767 # Location of Boulder
      # convert to map projection coords.
      # Note that lon,lat can be scalars, lists or numpy arrays.
      xpt,ypt = m(lon,lat)
      m.plot(xpt,ypt,'bo')  # plot a blue dot there
-     for icity in range(len(cityName)):
-          plt.text(xpt[icity]+30000,ypt[icity]+20000,cityName[icity])
-     plt.show()
+     for icity, city in enumerate(cityName):
+          if city == 'KBUF':
+               xoff, yoff = 20000, -40000
+          elif city == 'CYOW':
+               xoff, yoff = -100000, 23000
+          else:
+               xoff, yoff = 20000, 20000
+          plt.text(xpt[icity]+xoff,ypt[icity]+yoff, \
+                   city, \
+                   fontweight='bold', \
+                   fontsize='medium', \
+                   color='navy')
+     if showIt:
+          plt.show()
 
 ###############################################################
 ################# ADVECTION ARROWS ON MAP #####################
